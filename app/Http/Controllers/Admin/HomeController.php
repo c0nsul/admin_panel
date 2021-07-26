4891437e3cfc42fc305f6179643b810f7a0fb8a5
ConsuL <consul.developer@gmail.com>
2021-07-26 17:33:54 +0300

    delete

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -11,9 +12,11 @@ class HomeController extends Controller
     public function index()
     {
         $postsCount = Post::all()->count();
+        $categories_count = Category::all()->count();
 
         return view('admin.home.index', [
             'posts_count' => $postsCount,
+            'categories_count'=>$categories_count
         ]);
     }
 }
